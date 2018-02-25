dc2ab3f55bc1b45c7657e004cab560880dc13d7b
Shanvith Madhirala <shanvith@nwk-150-128.nwk.ruw.rutgers.edu>
2018-02-24 19:14:01 -0500

    basic frontend

--- webapp.py
+++ webapp.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request
+import get_info
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('home.html')
+	images = get_info.get_all_images()
+	return render_template('home.html', images=images)
 
 
-@app.route('/average')
+
+@app.route('/average', methods=["POST", "GET"])
 def average():
-	return render_template('home.html', images=images, average=average)
+	images = get_info.get_all_images()
+	average = get_info.get_average()
+	return render_template('average.html', images=images, average=average)
 
 if __name__ == '__main__':
 	app.run(debug=True)
